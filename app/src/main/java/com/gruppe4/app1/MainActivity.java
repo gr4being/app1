4b701ce0255c2f4e9a31c69a579a09fd9bd829a4
Alex <gr4being@gmail.com>
2022-03-30 13:42:52 +0200

    umm

--- app/src/main/java/com/gruppe4/app1/MainActivity.java
+++ app/src/main/java/com/gruppe4/app1/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.core.app.ActivityCompat;
 
@@ -18,6 +19,8 @@ import com.aldebaran.qi.sdk.builder.AnimateBuilder;
 import com.aldebaran.qi.sdk.builder.AnimationBuilder;
 import com.aldebaran.qi.sdk.builder.ChatBuilder;
 import com.aldebaran.qi.sdk.builder.EnforceTabletReachabilityBuilder;
+import com.aldebaran.qi.sdk.builder.ListenBuilder;
+import com.aldebaran.qi.sdk.builder.PhraseSetBuilder;
 import com.aldebaran.qi.sdk.builder.QiChatbotBuilder;
 import com.aldebaran.qi.sdk.builder.SayBuilder;
 import com.aldebaran.qi.sdk.builder.TopicBuilder;
@@ -26,6 +29,10 @@ import com.aldebaran.qi.sdk.object.actuation.Animate;
 import com.aldebaran.qi.sdk.object.actuation.Animation;
 import com.aldebaran.qi.sdk.object.actuation.EnforceTabletReachability;
 import com.aldebaran.qi.sdk.object.conversation.Chat;
+import com.aldebaran.qi.sdk.object.conversation.Listen;
+import com.aldebaran.qi.sdk.object.conversation.ListenResult;
+import com.aldebaran.qi.sdk.object.conversation.Phrase;
+import com.aldebaran.qi.sdk.object.conversation.PhraseSet;
 import com.aldebaran.qi.sdk.object.conversation.QiChatbot;
 import com.aldebaran.qi.sdk.object.conversation.Say;
 import com.aldebaran.qi.sdk.object.conversation.Topic;
@@ -33,6 +40,7 @@ import com.aldebaran.qi.sdk.object.locale.Language;
 import com.aldebaran.qi.sdk.object.locale.Locale;
 import com.aldebaran.qi.sdk.object.locale.Region;
 
+import java.lang.reflect.Array;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -42,6 +50,13 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
     private Activity mainActivity;
     private QiContext qiContext;
     private Animate animate;
+    private String question1;
+    private String question2;
+    private String question3;
+    private String question4;
+    private String question5;
+    public String[] a_ar = new String[]{};
+
     private Chat chat;
 
 
@@ -58,23 +73,46 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
         return question_dict;
     }
 
-    public void renameButtons(Hashtable<Integer, String> question_dict){
+    private void changeText(String text){
+        TextView textView  = findViewById(R.id.mytextview_id);
+        textView.setText(text);
+    }
+
+    private void arrayThing(String[] ar){
+        question1 = ar[1];
+        question2 = ar[2];
+        question3 = ar[3];
+        question4 = ar[4];
+        question5 = ar[5];
+    }
+
+    public void renameButtons(String[] question_dict){
         Log.i("aa", "about to run the while loop");
-        Enumeration<Integer> e = question_dict.keys();
-        while (e.hasMoreElements()) {
-            int key = e.nextElement();
-            Log.i("a", ": " + key + question_dict.get(key));
-        }
+        //Enumeration<Integer> e = question_dict.keys();
+        //while (e.hasMoreElements()) {
+        //    int key = e.nextElement();
+        //    Log.i("a", ": " + key + question_dict.get(key));
+        //}
         Button btn_1 = (Button)findViewById(R.id.btn_question_1);
-        btn_1.setText(question_dict.get(1));
+        btn_1.setText(question_dict[0]);
         Button btn_2 = (Button)findViewById(R.id.btn_question_2);
-        btn_2.setText(question_dict.get(2));
+        btn_2.setText(question_dict[1]);
         Button btn_3 = (Button)findViewById(R.id.btn_question_3);
-        btn_3.setText(question_dict.get(3));
+        btn_3.setText(question_dict[2]);
         Button btn_4 = (Button)findViewById(R.id.btn_question_4);
-        btn_4.setText(question_dict.get(4));
+        btn_4.setText(question_dict[3]);
         Button btn_5 = (Button)findViewById(R.id.btn_question_5);
-        btn_5.setText(question_dict.get(5));
+        btn_5.setText(question_dict[4]);
+    }
+
+    private void sayText(QiContext qiContext, String text){
+        Locale locale = new Locale(Language.GERMAN, Region.GERMANY);
+        Phrase phrase = new Phrase(text);
+        Future<Say> sayBuilding = SayBuilder.with(qiContext)
+                .withPhrase(phrase)
+                .withLocale(locale)
+                .buildAsync();
+        Future<Void> sayActionFuture = sayBuilding.andThenCompose(say -> say.async().run());
     }
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,6 +120,8 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
         QiSDK.register(this, this);
         mainActivity=this;
 
+        String[] ar = new String[]{"a", "b", "c", "d", "e"};
+        a_ar = new String[]{"no", "yes", "etc", "idgaf", "sure m8"};
         setContentView(R.layout.activity_main);
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
@@ -91,10 +131,8 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
         question_dict = createHasmap("wo ist mein geld!?!?!?", "can you yeet yourself",
                 "git mir mein geld zurueck", "eaaat shit", "wanna fight?");
 
-        renameButtons(question_dict);
-
+        renameButtons(ar);
     }
-
 
     protected void onDestroy() {
         QiSDK.unregister(this, this);
@@ -104,26 +142,40 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
     public void onRobotFocusGained(QiContext qiContext) {
         Locale locale = new Locale(Language.GERMAN, Region.GERMANY);
 
-        Say sayActionFuture = SayBuilder.with(qiContext)
-                .withText("Hallo! Gruppe 4.")
-                .withLocale(locale)
-                .build();
-        sayActionFuture.run();
+        sayText(qiContext, "Hallo, Gruppe 4");
 
-        Animation animation2 = AnimationBuilder.with(qiContext)
-                .withResources(R.raw.kratzen)
+
+        Animation animation = AnimationBuilder.with(qiContext)
+                .withResources(R.raw.stand3)
                 .build();
         animate = AnimateBuilder.with(qiContext)
-                .withAnimation(animation2)
+                .withAnimation(animation)
                 .build();
-        animate.addOnStartedListener(() -> Log.i("animation hype",
-                "Animation started."));
+        animate.addOnStartedListener(() -> Log.i("animation hype", "Animation started."));
 
         Button test_button = (Button) findViewById(R.id.btn_test);
         test_button.setOnClickListener(new View.OnClickListener() {
             public void onClick (View view) {
                 Log.i("aa", "button clicked");
                 Future<Void> animateFuture = animate.async().run();
+                View a = findViewById(R.id.btn_question_1);
+                a.setVisibility(View.VISIBLE);
+                View b = findViewById(R.id.btn_question_2);
+                b.setVisibility(View.VISIBLE);
+                View c = findViewById(R.id.btn_question_3);
+                c.setVisibility(View.VISIBLE);
+                View d = findViewById(R.id.btn_question_4);
+                d.setVisibility(View.VISIBLE);
+                View e = findViewById(R.id.btn_question_5);
+                e.setVisibility(View.VISIBLE);
+            }
+        });
+        Button question_button_1 = (Button) findViewById(R.id.btn_question_1);
+        question_button_1.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View view) {
+                Log.i("aa", "button question 1 clicked");
+                sayText(qiContext, a_ar[0]);
+                changeText(a_ar[0]);
             }
         });
 
@@ -131,25 +183,24 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
         // chat code
         // ---------------
 
-        Topic topic = TopicBuilder.with(qiContext)
-                .withResource(R.raw.test).build();
-        QiChatbot qiChatbot = QiChatbotBuilder.with(qiContext)
-                .withTopic(topic)
+        PhraseSet phraseSet = PhraseSetBuilder.with(qiContext)
+                .withTexts("Hello")
                 .build();
 
-        chat = ChatBuilder.with(qiContext)
-                .withChatbot(qiChatbot)
+        // Build the action.
+        Listen listen = ListenBuilder.with(qiContext)
+                .withPhraseSet(phraseSet)
                 .build();
-        chat.addOnStartedListener(() -> Log.i("aa", "Discussion started."));
-        Future<Void> chatFuture = chat.async().run();
 
-        chatFuture.thenConsume(future -> {
-            if (future.hasError()) {
-                Log.e("chat error hype",
-                        "Discussion finished with error.",
-                        future.getError());
-            }
-        });
+        // Run the action synchronously.
+        ListenResult listenResult = listen.run();
+        //((TextView)findViewById(R.id.mytextview_id)).setText(listenResult.getHeardPhrase().getText());
+        changeText(listenResult.getHeardPhrase().getText());
+        Log.i("aa", "Heard phrase: " + listenResult.getHeardPhrase().getText());
+
+        //View b = findViewById(R.id.btn_test);
+        //b.setVisibility(View.INVISIBLE);
+
     }
 
     public void onRobotFocusLost() {
